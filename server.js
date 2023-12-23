18f027cb4b70edd7968b41a16efd630e338e2e21
Mantas Mozuraitis <mozmantas@gmail.com>
2023-12-23 19:00:20 +0000

    successfully connect money exchange API

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 import express from "express"
 import pg from "pg"
 import bodyParser from "body-parser"
+import axios from "axios"
+import dotenv from "dotenv"
 
+// Express server variables
 const app = express();
 const port = 3000;
+
+// ENV file
+dotenv.config();
+
+// Currency API authentication
+const currencyApiKey = process.env.CURRENCY_API_KEY;
+const currencyApiUrl = "https://v6.exchangerate-api.com/v6/";
 
 // Location of static files
 app.use(express.static("public"));
@@ -52,6 +62,13 @@ app.post("/", async (req,res)=>{
     res.render("index.ejs", sorted!=null?{transactions:sorted, balance:balance}:{});
 })
 
+// API request for currency exchange rate
+app.post("/currency-exchange", async (req,res)=>{
+    const currency = "GBP";
+    await getCurrencyRate(currency, req.body.currency);
+    res.redirect("/");
+})
+
 app.listen(port, (req,res)=>{
     console.log(`Server is listening on port: ${port}`);
 }) 
@@ -95,5 +112,19 @@ async function deleteTransaction (id){
         console.log(`Transaction with id = ${id} has been deleted`);
     } catch (error) {
         console.error("Error: ", error.message); 
+    }
+}
+
+// API QUERY FUNCTIONS
+
+async function getCurrencyRate(currency, convert_to){
+    const url = `${currencyApiUrl}${currencyApiKey}/pair/${currency}/${convert_to}`;
+    try {
+        const response = await axios.get((url));
+        console.log("API request was successful");
+        return response.data.conversion_rate;
+    } catch (error) {
+        console.error("Error: ", error.message); 
+        return null;
     }
 }
